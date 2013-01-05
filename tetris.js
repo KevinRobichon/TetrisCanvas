8dec1af8b83f90d74bcde82a2042ba39a397d6fa
KevinRobichon <kevin.robichon@gmail.com>
2013-01-05 17:39:10 +0100

    scene object

--- tetris.js
+++ tetris.js
@@ -1,6 +1,11 @@
 var Tetris = function(canvasElement) {
     this.canvas = canvasElement;
     this.ctx = this.canvas.getContext('2d');
+    this.scene = new Scene(10, 20);
+}
+
+Tetris.prototype.render = function() {
+    scene.render();
 }
 
 Tetris.prototype.frame = function() {
